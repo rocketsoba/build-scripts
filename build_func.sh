6d4ed05d1220e8555efc0eb6ab080ddce25e9dc9
rockyakisoba <mmtrokuroku96@gmail.com>
2019-06-22 13:26:19 +0900

    リソースリストの取得部分関数化

---
+++ build_func.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+fetch_github_releases () {
+    # 引数の確認
+    if [ $# -ne 2 ]; then
+        echo "Usage: fetch_github_releases github_url target_path"
+        exit 1
+    fi
+    local TARGET_URL=$1
+    local TARGET_PATH=$2
+
+    # URLの確認
+    if !(echo $TARGET_URL | grep -P "^https\://github\.com/[^/]+/[^/]+/releases$" > /dev/null 2>&1); then
+        echo "Given URL is invalid. This function only accepts github release page URL."
+        exit 1
+    fi
+
+    # パスの確認
+    if [ ${TARGET_PATH:$((${#TARGET_PATH}-1))} != "/" ]; then
+       TARGET_PATH=${TARGET_PATH}"/"
+    fi
+    if [ "${TARGET_PATH:0:5}" != "/tmp/" ]; then
+        echo "Given PATH is invalid. Build path should be under \"/tmp\""
+        exit 1
+    fi
+    # ディレクトリが存在しなければ生成
+    if ! [ -d $TARGET_PATH ]; then
+        mkdir -p $TARGET_PATH
+    fi
+
+    # リソースのHTMLを取得
+    curl -Lso ${TARGET_PATH}releases.html $TARGET_URL
+
+    #リリースのHTMLが正しく取得で来てるか確認する
+    if ! [ -s ${TARGET_PATH}releases.html ] || ! [ -e ${TARGET_PATH}releases.html ]; then
+        echo "Fetch failed."
+        exit 1
+    fi
+
+    grep -Po "(?<=class=\"muted-link\" href=\").+\.tar\.gz" ${TARGET_PATH}releases.html | xargs -I{} echo "https://github.com"{} > ${TARGET_PATH}tarball_list
+    if ! [ -s ${TARGET_PATH}tarball_list ] || ! [ -e ${TARGET_PATH}tarball_list ]; then
+        echo "Unable to find archive link elements. Html dom might be changed."
+        exit 1
+    fi
+
+    echo "before while loop"
+
+    while true
+    do
+        sleep 5
+        local NEXT_URL=$(grep -Po "(?<=<a rel=\"nofollow\" href=\")[^\"]+(?=\">Next</a>)" ${TARGET_PATH}releases.html)
+
+        if [ "${NEXT_URL}" = "" ]; then
+            break
+        fi
+
+        if !(echo $NEXT_URL | grep -P "^https\://github\.com/[^/]+/[^/]+/releases" > /dev/null 2>&1); then
+            echo "Pagination move failed"
+            exit 1
+        fi
+
+        curl -Lso ${TARGET_PATH}releases.html $NEXT_URL
+
+        #リリースのHTMLが正しく取得で来てるか確認する
+        if ! [ -s ${TARGET_PATH}releases.html ] || ! [ -e ${TARGET_PATH}releases.html ]; then
+            echo "Fetch failed."
+            exit 1
+        fi
+
+        local RESULT=$(grep -Po "(?<=class=\"muted-link\" href=\").+\.tar\.gz" ${TARGET_PATH}releases.html | xargs -I{} echo "https://github.com"{})
+        if [ -z "${RESULT}" ]; then
+            echo "Unable to find archive link elements. Html dom might be changed."
+            exit 1
+        else
+            echo "${RESULT}" >> ${TARGET_PATH}tarball_list
+        fi
+    done
+}

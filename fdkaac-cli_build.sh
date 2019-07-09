4e91a021f3cace7d652bf54bf809e3cd071e53df
rockyakisoba <mmtrokuroku96@gmail.com>
2019-07-09 21:01:14 +0900

    コマンド置換をに変えた

--- fdkaac-cli_build.sh
+++ fdkaac-cli_build.sh
@@ -18,27 +18,27 @@ else
     if [ ${FDKAAC_PATH:$((${#FDKAAC_PATH}-1))} != "/" ]; then
         FDKAAC_PATH=${FDKAAC_PATH}"/"
     fi
-    LDFLAGS=`env PKG_CONFIG_PATH=${FDKAAC_PATH}lib/pkgconfig pkg-config --libs-only-L fdk-aac`
-    LDFLAGS=$LDFLAGS" -Wl,-rpath,"`find ${FDKAAC_PATH} -name 'lib' | head -n1`
-    CFLAGS=`env PKG_CONFIG_PATH=${FDKAAC_PATH}lib/pkgconfig pkg-config --cflags fdk-aac`
+    LDFLAGS=$(env PKG_CONFIG_PATH=${FDKAAC_PATH}lib/pkgconfig pkg-config --libs-only-L fdk-aac)
+    LDFLAGS=$LDFLAGS" -Wl,-rpath,"$(find ${FDKAAC_PATH} -name 'lib' | head -n1)
+    CFLAGS=$(env PKG_CONFIG_PATH=${FDKAAC_PATH}lib/pkgconfig pkg-config --cflags fdk-aac)
 fi
 
 echo $LDFLAGS
 echo $CFLAGS
 
-FDKAAC_CLI_BUILD_PATH=${WORK_PATH}"/fdkaac-cli-build."`date "+%Y%m%d%H%M%S."`$$"/"
+FDKAAC_CLI_BUILD_PATH=${WORK_PATH}"/fdkaac-cli-build."$(date "+%Y%m%d%H%M%S.")$$"/"
 FDKAAC_CLI_RELEASES_URL="https://github.com/nu774/fdkaac/releases"
 
 fetch_github_releases $FDKAAC_CLI_RELEASES_URL $FDKAAC_CLI_BUILD_PATH
 
-RESOURCE_URL=`cat ${FDKAAC_CLI_BUILD_PATH}tarball_list | head -n1`
-FDKAAC_CLI_VERSION=`echo $RESOURCE_URL | grep -Po '(?<=/v)[^/]+(?=\.tar\.gz)'`
+RESOURCE_URL=$(cat ${FDKAAC_CLI_BUILD_PATH}tarball_list | head -n1)
+FDKAAC_CLI_VERSION=$(echo $RESOURCE_URL | grep -Po '(?<=/v)[^/]+(?=\.tar\.gz)')
 FDKAAC_CLI_SRC=${FDKAAC_CLI_BUILD_PATH}"fdkaac-cli"${FDKAAC_VERSION}".tar.gz"
 
 curl -Lso $FDKAAC_CLI_SRC $RESOURCE_URL
 tar xf $FDKAAC_CLI_SRC -C $FDKAAC_CLI_BUILD_PATH
 
-cd $FDKAAC_CLI_BUILD_PATH`tar ft $FDKAAC_CLI_SRC | head -n1`
+cd $FDKAAC_CLI_BUILD_PATH$(tar ft $FDKAAC_CLI_SRC | head -n1)
 autoreconf -i
 env LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" ./configure --prefix=${FDKAAC_CLI_BUILD_PATH}"fdkaac-cli"
 make -j4
--- fdkaac_build.sh
+++ fdkaac_build.sh
@@ -11,19 +11,19 @@ else
     WORK_PATH=$TMP
 fi
 
-FDKAAC_BUILD_PATH=${WORK_PATH}"/fdkaac-build."`date "+%Y%m%d%H%M%S."`$$"/"
+FDKAAC_BUILD_PATH=${WORK_PATH}"/fdkaac-build."$(date "+%Y%m%d%H%M%S.")$$"/"
 FDKAAC_RELEASES_URL="https://github.com/mstorsjo/fdk-aac/releases"
 
 fetch_github_releases $FDKAAC_RELEASES_URL $FDKAAC_BUILD_PATH
 
-RESOURCE_URL=`cat ${FDKAAC_BUILD_PATH}tarball_list | head -n1`
-FDKAAC_VERSION=`echo $RESOURCE_URL | grep -Po '(?<=/v)[^/]+(?=\.tar\.gz)'`
+RESOURCE_URL=$(cat ${FDKAAC_BUILD_PATH}tarball_list | head -n1)
+FDKAAC_VERSION=$(echo $RESOURCE_URL | grep -Po '(?<=/v)[^/]+(?=\.tar\.gz)')
 FDKAAC_SRC=${FDKAAC_BUILD_PATH}"fdk-aac-"${FDKAAC_VERSION}".tar.gz"
 
 curl -Lso $FDKAAC_SRC $RESOURCE_URL
 tar xf $FDKAAC_SRC -C $FDKAAC_BUILD_PATH
 
-cd $FDKAAC_BUILD_PATH`tar ft $FDKAAC_SRC | head -n1`
+cd $FDKAAC_BUILD_PATH$(tar ft $FDKAAC_SRC | head -n1)
 autoreconf -i
 ./configure --prefix=${FDKAAC_BUILD_PATH}"fdk-aac" --enable-static=no --enable-static=yes
 make -j4
